003533113d388afdad51f17d28fb08124f981747
laszlo-v <hegyitaho@gmail.com>
2021-03-15 23:00:05 +0100

    try adding google sso

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,24 @@
 </template>
 
 <script>
+import * as Realm from "realm-web";
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  mounted() {
+    const app = new Realm.App("anywhere-backend-qcsqt");
+    const redirectUri = "http://localhost/login";
+    const credentials = Realm.Credentials.google(redirectUri)
+    app.logIn(credentials).then(user => {
+      // The logIn() promise will not resolve until you call `handleAuthRedirect()`
+      // from the new window after the user has successfully authenticated.
+      console.log(`Logged in with id: ${user.id}`);
+      Realm.handleAuthRedirect();
+    })
   }
 }
 </script>
